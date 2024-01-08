8f50c0538996d3f4c971cb209cc98a2e962e71a7
Soniamy <marianna.marzullo@gmail.com>
2024-01-08 11:08:09 +0100

    fine esercizio

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,8 @@ createApp({
     data() {
         return {
             saluto: 'Hello Vue!',
-            immagine: 'img-vue.jpg',
+            immagine:
+                'https://vueschool.io/storage/media/d54747c9b842f611b65d14cc865d403f/Common-Vue.js-Mistakes-and-How-to-Avoid-Them-01.png',
         };
     },
 }).mount('#app');
